06fdc11516aa9d228dcf32262b6326ec5b71e05a
Themba Biyela <thembabiyela20@gmail.com>
2025-05-02 18:38:26 +0200

    Changed url to page 1

--- client/src/services/movieService.ts
+++ client/src/services/movieService.ts
@@ -21,7 +21,7 @@ interface MovieApiResponse {
 
 export const getMovies = async (): Promise<Movie[]>=> {
 
-    const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=3&sort_by=popularity.desc';
+    const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
     const options = {
       method: 'GET',
       headers: {
